376d5cdf648011bdeaa1390f1d29a16fde610e47
Collins Quaye <104601285+Quaye007@users.noreply.github.com>
2022-07-16 20:44:26 +0000

    new commit
    added search feature

--- app.js
+++ app.js
@@ -27,27 +27,38 @@
 // }
 
 
+document.querySelector('#search-input').addEventListener('input', filterList);
 
 
+function filterList(){
+    const searchInput = document.querySelector('#search-input');
+    const filter = searchInput.value.toLowerCase();
+   
+   
+  
+    const listItems = document.querySelectorAll('.job-1');
+
+    listItems.forEach((item) => {
+        let text = item.textContent
+  
+
+        if(text.toLowerCase().includes(filter.toLowerCase())){
+            item.style.display = '';
+    
+        } else{
+            item.style.display = 'none';
+           
+        }
+    })
+   
 
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
+   
+    
+  
 
 
 
--- dark.js
+++ dark.js
@@ -16,8 +16,47 @@ var icon = document.getElementById("switch");
 
 icon.onclick = function(){
    document.body.classList.toggle("dark-theme")
-}
+};
 
 
 
 
+
+
+
+
+checkbox =document.getElementById('job-type');
+
+checkbox.addEventListener('change', e=>{
+    if(e.target.checked){
+        document.querySelector('#part-time-role').style.display ='none';
+    } else{
+        document.querySelector('#part-time-role').style.display ='';
+    }
+})
+
+
+
+
+// checkbox =document.getElementById('job-type');
+
+// checkbox.addEventListener('change', e=>{
+//     if(e.target.checked){
+//         document.querySelector('#part-time-role'[0]).style.display ='none';
+//     } else{
+//         document.querySelector('#part-time-role'[0]).style.display ='';
+//     }
+// })
+
+
+
+
+// function checker (){
+//     var checkbox = document.getElementById('job-type');
+
+//     if(checkbox.checked != true){
+//         document.querySelectorAll('#part-time-role').style.display ='';
+//     }else{
+//         document.querySelectorAll('#part-time-role').style.display ='none';
+//     }
+// }
